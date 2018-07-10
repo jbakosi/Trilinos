05e9e25421ee0b0f2af2615bf85abcf3674dc56e
Alejandro Mota <amota@sandia.gov>
2018-07-09 18:14:15 -0700

    Intrepid2, Shards: Support for composite surface.
    Add initial support for interpolation functions for a surface
    element compatible with the composite tetrahedron element.
    
    This also required adding a topology in Shards.

--- packages/intrepid2/src/Discretization/Basis/Intrepid2_HGRAD_WEDGE_COMP12_FEM.hpp
+++ packages/intrepid2/src/Discretization/Basis/Intrepid2_HGRAD_WEDGE_COMP12_FEM.hpp
@@ -40,24 +40,24 @@
 // ************************************************************************
 // @HEADER
 
-/** \file   Intrepid2_HGRAD_TET_COMP12_FEM.hpp
-    \brief  Header file for the Intrepid2::Basis_HGRAD_TET_COMP12_FEM class.
-    \author Created by P. Bochev, J. Ostien, K. Peterson and D. Ridzal.
+/** \file   Intrepid2_HGRAD_WEDGE_COMP12_FEM.hpp
+    \brief  Header file for the Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM class.
+    \author Created by P. Bochev and D. Ridzal
             Kokkorized by Kyungjoo Kim
 */
 
-#ifndef __INTREPID2_HGRAD_TET_COMP12_FEM_HPP__
-#define __INTREPID2_HGRAD_TET_COMP12_FEM_HPP__
+#ifndef __INTREPID2_HGRAD_WEDGE_COMP12_FEM_HPP__
+#define __INTREPID2_HGRAD_WEDGE_COMP12_FEM_HPP__
 
 #include "Intrepid2_Basis.hpp"
 
 namespace Intrepid2 {
-  
-  /** \class  Intrepid2::Basis_HGRAD_TET_COMP12_FEM
-      \brief  Implementation of the default H(grad)-compatible FEM basis of degree 2 on Tetrahedron cell
-  
-      Implements Lagrangian basis of degree 2 on the reference Tetrahedron cell. The basis has
-      cardinality 10 and spans a COMPLETE quadratic polynomial space. Basis functions are dual 
+
+  /** \class  Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM
+      \brief  Implementation of a H(grad)-compatible piecewise linear FEM basis on Wedge cell
+
+      Implements piecewise linear basis on the reference Wedge cell. The basis has
+      cardinality 12. Basis functions are dual
       to a unisolvent set of degrees-of-freedom (DoF) defined and enumerated as follows:
 
       \verbatim
@@ -66,80 +66,78 @@ namespace Intrepid2 {
       |   DoF   |----------------------------------------------------------|      DoF definition       |
       | ordinal |  subc dim    | subc ordinal | subc DoF ord |subc num DoF |                           |
       |=========|==============|==============|==============|=============|===========================|
-      |    0    |       0      |       0      |       0      |      1      |   L_0(u) = u(0,0,0)       |
+      |    0    |       0      |       0      |       0      |      1      |   L_0(u) = u( 0, 0,-1)    |
       |---------|--------------|--------------|--------------|-------------|---------------------------|
-      |    1    |       0      |       1      |       0      |      1      |   L_1(u) = u(1,0,0)       |
+      |    1    |       0      |       1      |       0      |      1      |   L_1(u) = u( 1, 0,-1)    |
       |---------|--------------|--------------|--------------|-------------|---------------------------|
-      |    2    |       0      |       2      |       0      |      1      |   L_2(u) = u(0,1,0)       |
+      |    2    |       0      |       2      |       0      |      1      |   L_2(u) = u( 0, 1,-1)    |
       |---------|--------------|--------------|--------------|-------------|---------------------------|
-      |    3    |       0      |       3      |       0      |      1      |   L_3(u) = u(0,0,1)       |
+      |    3    |       0      |       3      |       0      |      1      |   L_3(u) = u( 0, 0, 1)    |
       |---------|--------------|--------------|--------------|-------------|---------------------------|
+      |    4    |       0      |       4      |       0      |      1      |   L_4(u) = u( 1, 0, 1)    |
       |---------|--------------|--------------|--------------|-------------|---------------------------|
-      |    4    |       1      |       0      |       0      |      1      |   L_4(u) = u(1/2,0,0)     |
+      |    5    |       0      |       5      |       0      |      1      |   L_5(u) = u( 0, 1, 1)    |
       |---------|--------------|--------------|--------------|-------------|---------------------------|
-      |    5    |       1      |       1      |       0      |      1      |   L_5(u) = u(1/2,1/2,0)   |
+      |    6    |       1      |       0      |       0      |      1      |   L_6(u) = u(1/2, 0,-1)   |
       |---------|--------------|--------------|--------------|-------------|---------------------------|
-      |    6    |       1      |       2      |       0      |      1      |   L_6(u) = u(0,1/2,0)     |
+      |    7    |       1      |       1      |       0      |      1      |   L_7(u) = u(1/2,1/2,-1)  |
       |---------|--------------|--------------|--------------|-------------|---------------------------|
-      |    7    |       1      |       3      |       0      |      1      |   L_7(u) = u(0,0,1/2)     |
+      |    8    |       1      |       2      |       0      |      1      |   L_8(u) = u( 0,1/2,-1)   |
       |---------|--------------|--------------|--------------|-------------|---------------------------|
-      |    8    |       1      |       4      |       0      |      1      |   L_8(u) = u(1/2,0,1/2)   |
+      |    9    |       1      |       3      |       0      |      1      |   L_9(u) = u(1/2, 0, 1)   |
       |---------|--------------|--------------|--------------|-------------|---------------------------|
-      |    9    |       1      |       5      |       0      |      1      |   L_9(u) = u(0,1/2,1/2)   |
+      |   10    |       1      |       4      |       0      |      1      |   L_10(u)= u(1/2,1/2, 1)  |
+      |---------|--------------|--------------|--------------|-------------|---------------------------|
+      |   11    |       1      |       5      |       0      |      1      |   L_11(u)= u( 0,1/2, 1)   |
       |=========|==============|==============|==============|=============|===========================|
-      |   MAX   |  maxScDim=0  |  maxScOrd=3  |  maxDfOrd=0  |     -       |                           |
+      |   MAX   |  maxScDim=2  |  maxScOrd=8  |  maxDfOrd=0  |      -      |                           |
       |=========|==============|==============|==============|=============|===========================|
       \endverbatim
-  
-      \remark   Ordering of DoFs follows the node order in Tetrahedron<10> topology. Note that node order 
-      in this topology follows the natural order of k-subcells where the nodes are located, i.e.,
-      L_0 to L_3 correspond to 0-subcells (vertices) 0 to 3 and L_4 to L_9 correspond to
-      1-subcells (edges) 0 to 5.
+
   */
 
   namespace Impl {
 
     /**
-      \brief See Intrepid2::Basis_HGRAD_TET_COMP12_FEM
+      \brief See Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM
     */
-    class Basis_HGRAD_TET_COMP12_FEM {
+    class Basis_HGRAD_WEDGE_COMP12_FEM {
     public:
-      typedef struct Tetrahedron<11> cell_topology_type;
+      typedef struct Wedge<12> cell_topology_type;
 
       /**
-        \brief See Intrepid2::Basis_HGRAD_TET_COMP12_FEM
+        \brief See Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM
       */
       template<typename pointValueType>
       KOKKOS_INLINE_FUNCTION
       static ordinal_type
-      getLocalSubTet( const pointValueType x,
-                      const pointValueType y,
-                      const pointValueType z );
-      
+      getLocalSubTriangle( const pointValueType r,
+                           const pointValueType s );
+
       /**
-        \brief See Intrepid2::Basis_HGRAD_TET_COMP12_FEM
+        \brief See Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM
       */
       template<EOperator opType>
       struct Serial {
-        template<typename outputValueViewType,
-                 typename inputPointViewType>
+        template<typename outputViewType,
+                 typename inputViewType>
         KOKKOS_INLINE_FUNCTION
         static void
-        getValues(       outputValueViewType outputValues,
-                   const inputPointViewType  inputPoints );
-        
+        getValues(       outputViewType output,
+                   const inputViewType input );
+
       };
-      
-      template<typename ExecSpaceType, 
+
+      template<typename ExecSpaceType,
                typename outputValueValueType, class ...outputValueProperties,
                typename inputPointValueType,  class ...inputPointProperties>
       static void
-      getValues(        Kokkos::DynRankView<outputValueValueType,outputValueProperties...> outputValues,
-                  const Kokkos::DynRankView<inputPointValueType, inputPointProperties...>  inputPoints,
-                  const EOperator operatorType );
-      
+      getValues(       Kokkos::DynRankView<outputValueValueType,outputValueProperties...> outputValues,
+                 const Kokkos::DynRankView<inputPointValueType, inputPointProperties...>  inputPoints,
+                 const EOperator operatorType);
+
       /**
-        \brief See Intrepid2::Basis_HGRAD_TET_COMP12_FEM
+        \brief See Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM
       */
       template<typename outputValueViewType,
                typename inputPointViewType,
@@ -147,12 +145,12 @@ namespace Intrepid2 {
       struct Functor {
               outputValueViewType _outputValues;
         const inputPointViewType  _inputPoints;
-        
+
         KOKKOS_INLINE_FUNCTION
-        Functor(      outputValueViewType outputValues_,
-                      inputPointViewType  inputPoints_ )
+        Functor(       outputValueViewType outputValues_,
+                       inputPointViewType  inputPoints_ )
           : _outputValues(outputValues_), _inputPoints(inputPoints_) {}
-        
+
         KOKKOS_INLINE_FUNCTION
         void operator()(const ordinal_type pt) const {
           switch (opType) {
@@ -163,6 +161,7 @@ namespace Intrepid2 {
             break;
           }
           case OPERATOR_GRAD :
+          case OPERATOR_D2 :
           case OPERATOR_MAX : {
             auto       output = Kokkos::subview( _outputValues, Kokkos::ALL(), pt, Kokkos::ALL() );
             const auto input  = Kokkos::subview( _inputPoints,                 pt, Kokkos::ALL() );
@@ -172,19 +171,21 @@ namespace Intrepid2 {
           default: {
             INTREPID2_TEST_FOR_ABORT( opType != OPERATOR_VALUE &&
                                       opType != OPERATOR_GRAD &&
+                                      opType != OPERATOR_D2 &&
                                       opType != OPERATOR_MAX,
-                                      ">>> ERROR: (Intrepid2::Basis_HGRAD_TET_COMP12_FEM::Functor::operator() operator is not supported");
+                                      ">>> ERROR: (Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM::Serial::getValues) operator is not supported");
           }
           }
         }
       };
+
+
     };
   }
-
   template<typename ExecSpaceType = void,
            typename outputValueType = double,
            typename pointValueType = double>
-  class Basis_HGRAD_TET_COMP12_FEM : public Basis<ExecSpaceType,outputValueType,pointValueType> {
+  class Basis_HGRAD_WEDGE_COMP12_FEM : public Basis<ExecSpaceType,outputValueType,pointValueType> {
   public:
     typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_1d_host ordinal_type_array_1d_host;
     typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_2d_host ordinal_type_array_2d_host;
@@ -192,76 +193,73 @@ namespace Intrepid2 {
 
     /** \brief  Constructor.
      */
-    Basis_HGRAD_TET_COMP12_FEM();
-    
+    Basis_HGRAD_WEDGE_COMP12_FEM();
+
     typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::outputViewType outputViewType;
     typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::pointViewType  pointViewType;
     typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::scalarViewType  scalarViewType;
 
     using Basis<ExecSpaceType,outputValueType,pointValueType>::getValues;
 
-    /** \brief  FEM basis evaluation on a <strong>reference Tetrahedron</strong> cell. 
-    
-        Returns values of <var>operatorType</var> acting on FEM basis functions for a set of
-        points in the <strong>reference Tetrahedron</strong> cell. For rank and dimensions of 
-        I/O array arguments see Section \ref basis_md_array_sec .
-  
-        \param  outputValues      [out] - rank-2 or 3 array with the computed basis values
-        \param  inputPoints       [in]  - rank-2 array with dimensions (P,D) containing reference points  
-        \param  operatorType      [in]  - operator applied to basis functions    
-        
-        For rank and dimension specifications of <var>ArrayScalar</var> arguments see \ref basis_array_specs
-    */
     virtual
     void
     getValues(       outputViewType outputValues,
                const pointViewType  inputPoints,
                const EOperator operatorType = OPERATOR_VALUE ) const {
 #ifdef HAVE_INTREPID2_DEBUG
+      // Verify arguments
       Intrepid2::getValues_HGRAD_Args(outputValues,
                                       inputPoints,
                                       operatorType,
                                       this->getBaseCellTopology(),
                                       this->getCardinality() );
 #endif
-      Impl::Basis_HGRAD_TET_COMP12_FEM::
+      Impl::Basis_HGRAD_WEDGE_COMP12_FEM::
         getValues<ExecSpaceType>( outputValues,
                                   inputPoints,
                                   operatorType );
     }
 
-    /** \brief  Returns spatial locations (coordinates) of degrees of freedom on a
-        <strong>reference Tetrahedron</strong>.
-
-        \param  DofCoords      [out] - array with the coordinates of degrees of freedom,
-        dimensioned (F,D)
-    */
     virtual
     void
     getDofCoords( scalarViewType dofCoords ) const {
 #ifdef HAVE_INTREPID2_DEBUG
       // Verify rank of output array.
       INTREPID2_TEST_FOR_EXCEPTION( dofCoords.rank() != 2, std::invalid_argument,
-                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_TET_COMP12_FEM::getDofCoords) rank = 2 required for dofCoords array");
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM::getDofCoords) rank = 2 required for dofCoords array");
       // Verify 0th dimension of output array.
       INTREPID2_TEST_FOR_EXCEPTION( static_cast<ordinal_type>(dofCoords.extent(0)) != this->getCardinality(), std::invalid_argument,
-                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_TET_COMP12_FEM::getDofCoords) mismatch in number of dof and 0th dimension of dofCoords array");
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM::getDofCoords) mismatch in number of dof and 0th dimension of dofCoords array");
       // Verify 1st dimension of output array.
       INTREPID2_TEST_FOR_EXCEPTION( dofCoords.extent(1) != this->getBaseCellTopology().getDimension(), std::invalid_argument,
-                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_TET_COMP12_FEM::getDofCoords) incorrect reference cell (1st) dimension in dofCoords array");
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM::getDofCoords) incorrect reference cell (1st) dimension in dofCoords array");
 #endif
       Kokkos::deep_copy(dofCoords, this->dofCoords_);
     }
 
     virtual
+    void
+    getDofCoeffs( scalarViewType dofCoeffs ) const {
+#ifdef HAVE_INTREPID2_DEBUG
+      // Verify rank of output array.
+      INTREPID2_TEST_FOR_EXCEPTION( dofCoeffs.rank() != 1, std::invalid_argument,
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM::getdofCoeffs) rank = 1 required for dofCoeffs array");
+      // Verify 0th dimension of output array.
+      INTREPID2_TEST_FOR_EXCEPTION( static_cast<ordinal_type>(dofCoeffs.extent(0)) != this->getCardinality(), std::invalid_argument,
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_WEDGE_COMP12_FEM::getdofCoeffs) mismatch in number of dof and 0th dimension of dofCoeffs array");
+#endif
+      Kokkos::deep_copy(dofCoeffs, 1.0);
+    }
+
+    virtual
     const char*
     getName() const {
-      return "Intrepid2_HGRAD_TET_COMP12_FEM";
+      return "Intrepid2_HGRAD_WEDGE_COMP12_FEM";
     }
 
   };
 }// namespace Intrepid2
 
-#include "Intrepid2_HGRAD_TET_COMP12_FEMDef.hpp"
+#include "Intrepid2_HGRAD_WEDGE_COMP12_FEMDef.hpp"
 
 #endif
